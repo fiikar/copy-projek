c49a9f5b01357217ff9e74ec5a777a6cbf30d79a
fiikar <zulfikarsyahid20@gmail.com>
2025-07-17 11:05:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,13 @@ if menu == "Catatan Kuliah":
         st.subheader(f"📘 Catatan untuk {st.session_state.selected_matkul} - {st.session_state.selected_Semester} {st.session_state.selected_blok}")
         st.write("Pilih Pertemuan:")
         col1, col2, col3 = st.columns(3)
-        if col1.button("Pertemuan 1"):
+        elif col1.button("Pertemuan 1"):
             st.session_state.selected_pertemuan = "Pertemuan 1"
             st.session_state.page = "materi"
-        if col2.button("Pertemuan 2"):
+        elif col2.button("Pertemuan 2"):
             st.session_state.selected_pertemuan = "Pertemuan 2"
             st.session_state.page = "materi"
-        if col3.button("Pertemuan 3"):
+        elif col3.button("Pertemuan 3"):
             st.session_state.selected_pertemuan = "Pertemuan 3"
             st.session_state.page = "materi"
 
